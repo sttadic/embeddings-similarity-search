b3e4ce9780db8b65671a0bcab14325131ba8f8b8
sttadic <stjepan.tadich@gmail.com>
2024-06-16 17:52:27 +0100

    Switch statement and methods to deal with each option defined within the Menu class, FileIO class created

--- src/ie/atu/sw/Menu.java
+++ src/ie/atu/sw/Menu.java
@@ -1,14 +1,52 @@
 package ie.atu.sw;
 // Static import
 import static java.lang.System.out;
+import java.util.Scanner;
 
 public class Menu {
+	private Scanner scan = new Scanner(System.in);
+	private FileIO f = new FileIO();
+	private boolean keepRunning = true;
 	
-	public Menu() {
-		options();
+	public void init() {
+		while (keepRunning) {
+			showOptions();
+			
+			int choice = Integer.parseInt(scan.next());
+			switch (choice) {
+				case 1 	-> embeddingFile();
+				case 2 	-> outputFile();
+				case 3 	-> wordOrText();
+				case 4 	-> configure();
+				case 7 	-> keepRunning = false;
+				default -> out.print("Invalid Selection! Please try again.");
+			}
+		}
+		out.println("Bye!");
+		
 	}
 	
-	public void options() {
+	private void embeddingFile() {
+		String file = "./word-embeddings.txt";
+		out.println("Please specify the path to the word embeddings file (default ./word-embeddings.txt): ");
+		if (scan.nextLine().length() >= 1) file = scan.nextLine();
+		
+		
+	}
+	
+	private void outputFile() {
+		
+	}
+
+	private void wordOrText() {
+	
+	}
+
+	private void configure() {
+	
+	}
+	
+	public void showOptions() {
 		out.println("************************************************************");
 		out.println("*     ATU - Dept. of Computer Science & Applied Physics    *");
 		out.println("*                                                          *");
@@ -25,4 +63,6 @@ public class Menu {
 		out.println("");
 		out.println("Select Option (1-?) >");
 	}
+	
+	
 }
